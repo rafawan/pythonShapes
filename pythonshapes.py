5ecf457636e6dfe5df9ffaae6cceffc0ed0c6077
rafawan <rawan@cps.edu>
2017-07-06 14:32:29 -0500

    Update pythonshapes.py

--- pythonshapes.py
+++ pythonshapes.py
@@ -12,20 +12,25 @@ mrTurtle.setposition(x_pos, y_pos)
 
 ### Write your code below:
 
+#user inputs
 sidesString = input("Enter the number of sides: ")
 sides = int(sidesString)
 color = input("Enter the outside color you want: ")
 fillColor = input("Enter the inside color you want: ")
 
+#set colors
 mrTurtle.pencolor(color)
 mrTurtle.fillcolor(fillColor)
 
+#begin fill and drawing
 mrTurtle.begin_fill()
 mrTurtle.pendown()
 
+#create shape
 for i in range(sides):
     mrTurtle.forward(100)
     mrTurtle.left(360/sides)
+    
 mrTurtle.penup()
 mrTurtle.end_fill()
 
